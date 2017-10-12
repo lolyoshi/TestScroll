de6c32b10452cfc756fe6e00dca3bd972f43d997
lolyoshi <nguyenlethubao@gmail.com>
2017-10-13 02:19:54 +0700

    - fix position

--- Assets/Script/ScrollList.cs
+++ Assets/Script/ScrollList.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScrollList : MonoBehaviour {
-	public GameObject prefab;
-	 public Transform contentPanel;
-	public int itemAmount;
+public class ScrollList : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform contentPanel;
+    public ScrollRect scrollView;
+    public int itemAmount;
 
-	// Use this for initialization
-	void Start () {
-		CreateItemList();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        CreateItemList();
+    }
 
-	void CreateItemList() {
-		for (int i = 0; i < itemAmount; ++i) {
-			GameObject itemObject = Instantiate(prefab) as GameObject;
-			Item item = itemObject.GetComponent<Item>();
-			item.itemImage.sprite = Resources.Load<Sprite>((i + 1).ToString());
-			item.transform.SetParent(contentPanel);
-		}
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+    }
+
+    void CreateItemList()
+    {
+        RectTransform rowRectTransform = prefab.GetComponent<RectTransform>();
+        RectTransform containerRectTransform = contentPanel.GetComponent<RectTransform>();
+
+        //adjust the height of the container so that it will just barely fit all its children
+        float scrollHeight = -rowRectTransform.rect.height * itemAmount;
+        containerRectTransform.sizeDelta = new Vector2(containerRectTransform.rect.width, scrollHeight);
+
+        for (int i = 0; i < itemAmount; ++i)
+        {
+            GameObject itemObject = Instantiate(prefab) as GameObject;
+            Item item = itemObject.GetComponent<Item>();
+            item.itemImage.sprite = Resources.Load<Sprite>((i + 1).ToString());
+            item.transform.SetParent(contentPanel);
+
+            //move and size the new item
+            RectTransform rectTransform = item.GetComponent<RectTransform>();
+            float y = rowRectTransform.rect.height * i + rowRectTransform.rect.height / 2;
+            Debug.Log("Y = " + y);
+            rectTransform.localPosition = new Vector3(rowRectTransform.rect.width / 2, y, 0);
+        }
+    }
 }
